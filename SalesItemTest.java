e83f32955409e79d88bb5a2296ca34b23acbda77
Richard <richardlife101@gmail.com>
2025-11-07 11:29:35 -0500

    19?

--- SalesItemTest.java
+++ SalesItemTest.java
@@ -71,5 +71,50 @@ public class SalesItemTest
     }
 
 
+
+    @Test
+    public void Testfindmosthelpful()
+    {
+        SalesItem salesIte1 = new SalesItem("Joe", 56);
+        Comment comment1 = new Comment("joe", "this book was really helpful", 5);
+        assertEquals("joe", comment1.getAuthor());
+        comment1.upvote();
+        comment1.upvote();
+        Comment comment2 = new Comment("joe", "this book sucks", 1);
+        comment2.upvote();
+        comment2.upvote();
+        comment2.upvote();
+        comment2.upvote();
+        comment2.upvote();
+        comment2.upvote();
+    }
+
+    @Test
+    public void Q19()
+    {
+        SalesItem salesIte1 = new SalesItem("Math", 400);
+        assertEquals(1, salesIte1.addComment("Really good", "this made me cry", 4));
+        assertEquals(0, salesIte1.getNumberOfComments());
+        salesIte1.upvoteComment(0);
+        assertEquals(50, salesIte1.getPrice());
+        assertEquals("really good", salesIte1.findMostHelpfulComment());
+        Comment comment1 = salesIte1.findMostHelpfulComment();
+        comment1.getAuthor();
+        comment1.getFullDetails();
+        comment1.getRating();
+        comment1.upvote();
+        comment1.upvote();
+        comment1.upvote();
+        salesIte1.findMostHelpfulComment();
+        Comment comment2 = new Comment("this sucks", "I hate this book", 1);
+        comment2.downvote();
+        comment2.downvote();
+        comment2.downvote();
+        Comment comment3 = salesIte1.findMostHelpfulComment();
+        comment3.getAuthor();
+        comment2.downvote();
+    }
 }
+
+
 
